7b314a2716ffe6cc40fa10d43b82055d04657768
Sohidul Islam Ananto <anontom90@gmail.com>
2025-09-04 16:40:40 +0600

    Update user.controller.ts

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,55 +1,134 @@
-import { NextFunction, Request, Response } from 'express';
+import bcrypt from 'bcrypt';
 import { StatusCodes } from 'http-status-codes';
-import catchAsync from '../../../shared/catchAsync';
-import { getSingleFilePath } from '../../../shared/getFilePath';
-import sendResponse from '../../../shared/sendResponse';
-import { UserService } from './user.service';
+import { model, Schema } from 'mongoose';
+import config from '../../../config';
+import { STATUS, USER_ROLES } from '../../../enums/user';
+import ApiError from '../../../errors/ApiError';
+import { IUser, UserModal } from './user.interface';
 
-const createUser = catchAsync(
-  async (req: Request | any, res: Response, next: NextFunction) => {
-    const { ...userData } = req.body;
-    const result = await UserService.createUserToDB(userData);
-
-    sendResponse(res, {
-      success: true,
-      statusCode: StatusCodes.OK,
-      message: 'User created successfully',
-      data: result,
-    });
-  }
+const userSchema = new Schema<IUser, UserModal>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: Object.values(USER_ROLES),
+      default: USER_ROLES.USER,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      match: [ 
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, 
+        'Please provide a valid email address' 
+      ],
+    },
+    contact:{
+      type: String,
+      default: '',
+    },
+    password: {
+      type: String,
+      required: true,
+      select: 0,
+      minlength: 8,
+    },
+    image: {
+      type: String,
+      default: '',
+    },
+    status: {
+      type: String,
+      enum: Object.values(STATUS),
+      default: STATUS.ACTIVE,
+    },
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+    authentication: {
+      isResetPassword: {
+        type: Boolean,
+        default: false,
+      },
+      oneTimeCode: {
+        type: Number,
+        default: null,
+      },
+      expireAt: {
+        type: Date,
+        default: null,
+      },
+    },
+  },
+  { timestamps: true }
 );
 
-const getUserProfile = catchAsync(async (req: Request | any, res: Response) => {
-  const user = req.user;
-  const result = await UserService.getUserProfileFromDB(user);
+//exist user check
+userSchema.statics.isExistUserById = async (id: string) => {
+  const isExist = await User.findById(id);
+  return isExist;
+};
 
-  sendResponse(res, {
-    success: true,
-    statusCode: StatusCodes.OK,
-    message: 'Profile data retrieved successfully',
-    data: result,
-  });
+userSchema.statics.isExistUserByEmail = async (email: string) => {
+  const isExist = await User.findOne({ email });
+  return isExist;
+};
+
+//is match password
+userSchema.statics.isMatchPassword = async (
+  password: string,
+  hashPassword: string
+): Promise<boolean> => {
+  return await bcrypt.compare(password, hashPassword);
+};
+
+//Check user With validation in shourt and return the user
+userSchema.statics.isValidUser = async (id: string) => {
+  const isExist = await User  
+                        .findById( id)
+                        .select("-password -authentication -__v -updatedAt -createdAt")
+                        .lean()
+                        .exec();
+
+  if (!isExist) {
+    throw new ApiError(
+      StatusCodes.NOT_FOUND,
+      "User not found"
+    );
+  };
+
+  if (!isExist.verified) {
+    throw new ApiError(
+      StatusCodes.FORBIDDEN,
+      "Your account was not verified!"
+    )
+  };
+
+  if (isExist.status !== STATUS.ACTIVE) {
+    throw new ApiError(
+      StatusCodes.NOT_ACCEPTABLE,
+      `You account was ${isExist.status}!`
+    );
+  };
+  return isExist;
+};
+
+userSchema.pre('save', async function (next) {
+  
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(
+      this.password,
+      Number(config.bcrypt_salt_rounds)
+    );
+  }
+
+  next();
 });
 
-//update profile
-const updateProfile = catchAsync(
-  async (req: Request | any, res: Response, next: NextFunction) => {
-    const user = req.user;
-    let image = getSingleFilePath(req.files, 'image');
-
-    const data = {
-      image,
-      ...req.body,
-    };
-    const result = await UserService.updateProfileToDB(user, data);
-
-    sendResponse(res, {
-      success: true,
-      statusCode: StatusCodes.OK,
-      message: 'Profile updated successfully',
-      data: result,
-    });
-  }
-);
-
-export const UserController = { createUser, getUserProfile, updateProfile };
+export const User = model<IUser, UserModal>('User', userSchema);
