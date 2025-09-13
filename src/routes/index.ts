fb9a6c27c21f18e885ee87dd11cb45380b3a4af0
Sohidul Islam Ananto <anontom90@gmail.com>
2025-09-13 15:54:53 +0600

    updated the endpient

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
-import { AuthRoutes } from '../app/modules/auth/auth.route';
-import { UserRoutes } from '../app/modules/user/user.route';
+import AuthRoutes from '../app/modules/auth/auth.route';
+import UserRoutes from '../app/modules/user/user.route';
 const router = express.Router();
 
 const apiRoutes = [
